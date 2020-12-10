6f7c0d4dd300bbb07e5eebcd0f53a5973b3e0484
Victoria Abramova <abramovaviktori@mail.ru>
2020-12-10 12:25:24 +0700

    new variants 4 ex

--- Lesson_2/4.py
+++ Lesson_2/4.py
@@ -1,9 +1,19 @@
-user_str = input('Напишите несколько слов, разделенных пробелом: ')
-new_list = user_str.split(' ')
-for i in new_list:
-    print(new_list.index(i) + 1, i[:10])
+# user_str = input('Напишите несколько слов, разделенных пробелом: ')
+# new_list = user_str.split(' ')
+# for i in new_list:
+#     print(new_list.index(i) + 1, i[:10])
+#
+# # ИЛИ
+#
+# for ind, i in enumerate(new_list):
+#     print(ind + 1, i[:10])
 
-# ИЛИ
+# user_str = input('Напишите несколько слов, разделенных пробелом: ').split()
+#
+# for n, i in enumerate(user_str, 1):
+#     print(n, i) if len(i) <= 10 else print(n, (i[:10]))
 
-for ind, i in enumerate(new_list):
-    print(ind + 1, i[:10])
+user_str = input('Напишите несколько слов, разделенных пробелом: ').split()
+
+for i, word in enumerate(user_str, 1):
+    print(f'{i} {word[:10]}')
