09a3ba161fab3dc4dbfb7a079bd794d87498557f
Victoria Abramova <abramovaviktori@mail.ru>
2020-12-10 12:26:08 +0700

    fix 5 ex

--- Lesson_2/5.py
+++ Lesson_2/5.py
@@ -1,14 +1,9 @@
 my_list = [7, 5, 3, 3, 2]
-new_el = int(input('Введите число: '))
+new_el = float(input('Введите число: '))
 
-for i in my_list:
-    if new_el > i:
-        my_list.insert(my_list.index(i), new_el)
-        break
-    if new_el == i:
-        my_list.insert(my_list.index(i)+1, new_el)
-        break
-    if new_el < i:
-        my_list.append(new_el)
-        break
+i = 0
+for n in my_list:
+    if new_el <= n:
+        i += 1
+my_list.insert(i, float(new_el))
 print(my_list)
