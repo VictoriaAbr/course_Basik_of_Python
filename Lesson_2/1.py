8093471825463cfff2b8a65cc2a6e849b5d69c46
Victoria Abramova <abramovaviktori@mail.ru>
2020-12-10 12:24:37 +0700

    update 1 ex

--- Lesson_2/1.py
+++ Lesson_2/1.py
@@ -2,5 +2,5 @@ my_list = [
     2, 'hello', [2, 3], 4.5, ('one', 'two'), {'1': 'one', '2': 'two'}, set('trampapam'), (b'digit'), bytearray(b'digits'), (None), #(1/0)
 ]
 
-for i in my_list:
-    print(type(i))
+for i, item in enumerate(my_list, 1):
+    print(f'{i} {item} - {type(item)}')
